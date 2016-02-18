d1f7625f34b65733144601af68d5214a60c3a391
kasldfh <forumf41l@gmail.com>
2016-02-18 12:19:36 -0600

    initial actual commit

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+io.on('connection', function(socket){
+  console.log('user connected');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
 });
 
-
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
