ecbe032672030c24891051234550fe988ff4f698
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2016-03-14 16:20:15 -0500

    clean up code documentation

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
 // 
-// Node.js Server Code
+// Whiteboard server
+// Codename 'Silver Surfer' (because we need a cool code name)
 // UMD CS 4995
 //
-// Code manages the traffic for whiteboards, along with webRTC video components of the interface.
-//
-// Message Types handled by the Server
-// ==================================
-//
-// log client - 
-// 
-// Important information for using the socket.io classes:
+// Code manages the traffic for whiteboards, along with (web)RTC signalling.
 // 
 // TODO ---
 //  Implement Loading Whiteboard
 //  Emit to specific client/clients
 //  Browser client for debugging?
+//  Implement MySQL safely
 // ---------
 
-// Express is a JS web app system 
-var app = require('express')(),	
-    path = require('path'),	
-    streams = require('./streams.js')();
-    
-var http = require('http').Server(app);
-var mainListeningSocket = require('socket.io')(http);
+// Load configuration from .env file
 require('dotenv').config();
 
-var favicon = require('serve-favicon'),
+var app = require('express')()
+    path = require('path'),	
+    streams = require('./streams.js')(),
+    http = require('http').Server(app),
+    mainListeningSocket = require('socket.io')(http),
+    favicon = require('serve-favicon'),
 	logger = require('morgan'),
 	methodOverride = require('method-override'),
     bodyParser = require('body-parser'),
 	errorHandler = require('errorhandler');
 
-//
 // MySQL connection information
-//
 var useMYSQL = false;
 if (useMYSQL) {
 	var mysql  = require('mysql');
@@ -47,12 +39,11 @@ if (useMYSQL) {
 	connection.connect();
 }
 
-// Ideally, add all the handlers to an array
+// TODO: Ideally, add all HTTP handlers to an array
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
-// GET streams as JSON
 app.get('/streams.json', function(req, res) {
   var streamList = streams.getStreams();
   // JSON exploit to clone streamList.public
@@ -62,10 +53,8 @@ app.get('/streams.json', function(req, res) {
 });
 
 // Array to hold the list of connected clients
-// - Stores the clientSocket.id that can be used to emit messages back to those clients.
-//
 var clientList = [];      // array of the client ids
-var whiteboardMap = {};  // array of the whiteboards
+var whiteboardMap = {};   // map of the whiteboards
 
 // socket.io entry point
 mainListeningSocket.on('connection', function(clientSocket){
@@ -75,6 +64,10 @@ mainListeningSocket.on('connection', function(clientSocket){
 
 	// Add the client socket identifier to the client array
 	clientList.push(clientSocket);
+    
+    // Emit the id back to the client so they know what their id is
+    // (currently only used by RTC code)
+    clientSocket.emit('id', clientSocket.id);
 
 	// createWhiteboard
     // creates a whiteboard with info from JSON data
@@ -102,7 +95,7 @@ mainListeningSocket.on('connection', function(clientSocket){
 	});
 
 	// joinWhiteboard - Client Protocol Message
-    // joins whitebaords by id
+    // joins whiteboards by id
 	clientSocket.on('joinWhiteboard', function(msg) {
 		console.log('joinWhiteboard', msg);
 
@@ -115,9 +108,7 @@ mainListeningSocket.on('connection', function(clientSocket){
 		clientSocket.emit('response', { 'status': 100, 'message': 'Joined whiteboard' });
 	});
 
-	// Our test messages....
-	//    chat message
-	//    motion event
+    // chat message - echoes the message to all connected clients
 	clientSocket.on('chat message', function(msg){
 
 		// No DB Stuff yet...
@@ -130,12 +121,11 @@ mainListeningSocket.on('connection', function(clientSocket){
 		// Because this goes out the main socket, all clients will get it...
 		mainListeningSocket.emit('chat message', msg);
 
-
 		// this may be another way to send to all connected sockets:
 		// mainListeningSocket.sockets.emit('messagename', 'message');
 	});
 
-    // Event triggered by client drawing
+    // motionevent - triggered by client drawing
 	clientSocket.on('motionevent', function(msg) {
 
 		// DB queries not yet reliable
@@ -146,9 +136,9 @@ mainListeningSocket.on('connection', function(clientSocket){
 		mainListeningSocket.emit('motionevent', msg);
 		console.log('motionevent', msg);
 	});
-    
-    clientSocket.emit('id', clientSocket.id);
 
+    // message - represents a direct message to another client
+    // (currently only used by RTC)
     clientSocket.on('message', function (details) {
       var otherClient = io.sockets.connected[details.to];
 
@@ -160,18 +150,22 @@ mainListeningSocket.on('connection', function(clientSocket){
         otherClient.emit('message', details);
     });
       
+    // readyToStream - adds a client stream to the master stream list
+    // (RTC only)
     clientSocket.on('readyToStream', function(options) {
       console.log('-- ' + clientSocket.id + ' is ready to stream --');
       
       streams.addStream(clientSocket.id, options.name); 
     });
     
+    // update - updates a client stream on the master stream list
+    // (RTC only)
     clientSocket.on('update', function(options) {
       streams.update(clientSocket.id, options.name);
     });
 
     //Handles all client disconnects
-    //Removes clients from our own client list
+    //Removes clients from our own client list and from the video stream list
     function leave() {
       console.log('Client left: ' + clientSocket.id);
       streams.removeStream(clientSocket.id);
@@ -179,14 +173,13 @@ mainListeningSocket.on('connection', function(clientSocket){
 
     clientSocket.on('disconnect', leave);
     clientSocket.on('leave', leave);
-
 });
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//
-//
+
+
 var port = process.env.PORT || 3000
 http.listen(port, function(){
 	console.log('listening on *:' + port);
 });
 
+// TODO: Here lies dead MySQL code
 //connection.end();
